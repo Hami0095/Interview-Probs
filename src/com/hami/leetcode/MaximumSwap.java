a454cb394144264efc22560888397b9e0c303449
Hami0095 <abdman0095@gmail.com>
2024-10-18 22:00:13 +0500

    Added the problem

--- src/com/hami/leetcode/MaximumSwap.java
+++ src/com/hami/leetcode/MaximumSwap.java
@@ -1,4 +1,29 @@
 package com.hami.leetcode;
+/*
+ * Author: Mohammad Abdur Rehman Cheema
+ * Date: 10/18/2024
+ * 
+ * PROBLEM => 
+		You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+		
+		Return the maximum valued number you can get.
+		
+		 
+		
+		Example 1:
+		
+		Input: num = 2736
+		Output: 7236
+		Explanation: Swap the number 2 and the number 7.
+		Example 2:
+		
+		Input: num = 9973
+		Output: 9973
+		Explanation: No swap.
+ * 
+ * 
+ * 
+ * */
 class MaximumSwap {
     public int maximumSwap(int num) {
         String numStr = Integer.toString(num);
